b48daf9ed8a68d0591b558f9cb99b93cfcf4ab1f
RadStr <radek.strycek@seznam.cz>
2025-08-02 12:26:49 +0200

    No need for new tryUP migration

--- services/backend/src/tools/migrations/index.ts
+++ services/backend/src/tools/migrations/index.ts
@@ -2,7 +2,6 @@ import m1 from './pim-to-semantic-model.ts';
 
 export const migrations = {
   1: m1,
-  2: m1       // TODO RadStr: Do I have to? I had to do it long time ago, but maybe I don't now
 } as Record<number, () => Promise<void>>;
 
-export const currentVersion = 2;
+export const currentVersion = 1;
