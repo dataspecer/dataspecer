375406be09233bbe8a326927581da61a6599e8ef
Petr Škoda <skodapetr@gmail.com>
2025-09-06 22:18:55 +0200

    Fix implementation of InMemory models

--- packages/profile-model/src/in-memory/writable-in-memory-model.ts
+++ packages/profile-model/src/in-memory/writable-in-memory-model.ts
@@ -2,58 +2,31 @@ import {
   InMemorySemanticModel,
 } from "@dataspecer/core-v2/semantic-model/in-memory";
 import {
-  ProfileEntityRecord,
-  ProfileOperation,
-  ProfileOperationResult,
   WritableProfileModel,
 } from "../profile-model.ts";
 
-class WritableInMemoryProfileModel implements WritableProfileModel {
+class WritableInMemoryProfileModel
+  extends InMemorySemanticModel
+  implements WritableProfileModel {
 
   readonly identifier: string;
-
-  /**
-   * We keep copy of the baseIri to enable for null as a value.
-   */
-  readonly baseIri: string | null;
-
-  readonly entities: ProfileEntityRecord = {};
-
-  readonly wrapper: InMemorySemanticModel;
 
   constructor(
     identifier: string,
     baseIri: string | null,
   ) {
+    super();
     this.identifier = identifier;
-    this.baseIri = baseIri;
-    //
-    this.wrapper = new InMemorySemanticModel();
-    if (baseIri !== null) {
-      this.wrapper.setBaseIri(baseIri);
-    }
+    this.setBaseIri(baseIri ?? "");
   }
 
   getId(): string {
     return this.identifier;
   }
 
-  getEntities(): ProfileEntityRecord {
-    return this.wrapper.getEntities();
-  }
-
-  getBaseIri(): string | null {
-    return this.baseIri;
-  }
-
-  executeOperations(operations: ProfileOperation[])
-    : ProfileOperationResult[] {
-    return this.wrapper.executeOperations(operations);
-  }
-
 }
 
-export function createWritableInMemoryProfileModel(args:{
+export function createWritableInMemoryProfileModel(args: {
   identifier: string,
   baseIri: string | null,
 }): WritableProfileModel {
--- packages/semantic-model/src/in-memory/writable-in-memory-model.ts
+++ packages/semantic-model/src/in-memory/writable-in-memory-model.ts
@@ -2,53 +2,26 @@ import {
   InMemorySemanticModel,
 } from "@dataspecer/core-v2/semantic-model/in-memory";
 import {
-  SemanticEntityRecord,
-  SemanticOperation,
-  SemanticOperationResult,
   WritableSemanticModel,
 } from "../semantic-model.ts";
 
-class WritableInMemorySemanticModel implements WritableSemanticModel {
+class WritableInMemorySemanticModel
+  extends InMemorySemanticModel
+  implements WritableSemanticModel {
 
   readonly identifier: string;
-
-  /**
-   * We keep copy of the baseIri to enable for null as a value.
-   */
-  readonly baseIri: string | null;
-
-  readonly entities: SemanticEntityRecord = {};
-
-  readonly wrapper: InMemorySemanticModel;
 
   constructor(
     identifier: string,
     baseIri: string | null,
   ) {
+    super();
     this.identifier = identifier;
-    this.baseIri = baseIri;
-    //
-    this.wrapper = new InMemorySemanticModel();
-    if (baseIri !== null) {
-      this.wrapper.setBaseIri(baseIri);
-    }
+    this.setBaseIri(baseIri ?? "");
   }
 
   getId(): string {
     return this.identifier;
-  }
-
-  getEntities(): SemanticEntityRecord {
-    return this.wrapper.getEntities();
-  }
-
-  getBaseIri(): string | null {
-    return this.baseIri;
-  }
-
-  executeOperations(operations: SemanticOperation[])
-    : SemanticOperationResult[] {
-    return this.wrapper.executeOperations(operations);
   }
 
 }
