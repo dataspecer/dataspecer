659627997c7bbb082b2cafd2007337233810aecc
Petr Škoda <skodapetr@gmail.com>
2025-09-07 11:55:06 +0200

    Move enums to profile-model package

--- packages/profile-model/src/index.ts
+++ packages/profile-model/src/index.ts
@@ -3,4 +3,5 @@ export * from "./in-memory/index.ts";
 export * from "./default-profile-model-builder.ts";
 export * from "./profile-model-builder.ts";
 export * from "./profile-model.ts";
+export * from "./profile-well-known.ts";
 export * from "./utilities.ts";
