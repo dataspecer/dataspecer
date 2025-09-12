3b2a95963bf3ac9e038ac06f16904b2d72d04ec5
RadStr <radek.strycek@seznam.cz>
2025-09-12 08:42:23 +0200

    Update manager on git commit

--- applications/manager/src/dialog/git-url.tsx
+++ applications/manager/src/dialog/git-url.tsx
@@ -238,6 +238,7 @@ export const commitToGitDialogOnClickHandler = async (openModal: OpenBetterModal
         method: "GET",
       });
     gitOperationResultToast(response);
+    requestLoadPackage(iri, true);
   }
 };
 
