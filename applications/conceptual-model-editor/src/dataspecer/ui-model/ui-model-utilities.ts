af373dba6541dfeefded92df62e8190202f1efc6
Petr Škoda <skodapetr@gmail.com>
2025-12-27 21:34:50 +0100

    Remove unused files

--- applications/conceptual-model-editor/src/dataspecer/ui-model/ui-model-utilities.ts
+++
@@ -1,8 +0,0 @@
-import { UiCardinality } from "./model";
-
-export const uiCardinalityToString = (cardinality: UiCardinality | null): string | null => {
-  if (cardinality === null || cardinality === undefined) {
-    return null;
-  }
-  return `[${cardinality[0] ?? "*"}..${cardinality[1] ?? "*"}]`;
-};
