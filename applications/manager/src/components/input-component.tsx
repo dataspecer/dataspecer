69d539dddfe49fec7f332d9f151d37b2c6d940a0
RadStr <radek.strycek@seznam.cz>
2026-03-02 15:18:35 +0100

    Add tooltip prop to InputComponent

--- applications/manager/src/components/input-component.tsx
+++ applications/manager/src/components/input-component.tsx
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label"
 import { RefObject } from "react";
+import { PopOverGitGeneralComponent } from "./popover-git-general";
 
 type SetStringState = (value: string) => void;
 
@@ -19,24 +20,28 @@ export type InputComponentProps = {
   input?: string;
   label?: string;
   tooltip?: string;
+  TooltipComponent?: React.ReactNode;
 };
 
 export function createIdentifierForHTMLElement(idPrefix: string, idSuffix: number, htmlElementType: string) {
   return `${idPrefix}-${idSuffix}-${htmlElementType}`;
 }
 
-export const InputComponent = ({ idPrefix, idSuffix, input, label, tooltip, setInput, disabled, requiredRefObject }: InputComponentProps) => {
+export const InputComponent = ({ idPrefix, idSuffix, input, label, tooltip, TooltipComponent, setInput, disabled, requiredRefObject }: InputComponentProps) => {
   const divId = createIdentifierForHTMLElement(idPrefix, idSuffix, "div");
   const inputId = createIdentifierForHTMLElement(idPrefix, idSuffix, "input");
 
   return <div className="grid gap-4 my-4">
     <div title={tooltip} key={divId}>
-      <Label htmlFor={inputId} className="flex grow-3 items-baseline gap-2 mb-2">
-        <div>
-          {label } {requiredRefObject === undefined ? null : <span className="text-red-500">*</span>}
-        </div>
-        <div className="grow"></div>
-      </Label>
+      <div className="flex flex-row">
+        <Label htmlFor={inputId} className="flex grow-3 items-baseline gap-2 mb-2">
+          <div>
+            {label } {requiredRefObject === undefined ? null : <span className="text-red-500">*</span>}
+          </div>
+          <div className="grow"></div>
+        </Label>
+        {TooltipComponent === undefined ? null : <PopOverGitGeneralComponent>{TooltipComponent}</PopOverGitGeneralComponent>}
+      </div>
       <Input ref={requiredRefObject} id={inputId} value={input} className="grow my-1" onChange={target => setInput(target.target.value)} disabled={disabled ?? false} required={requiredRefObject !== undefined}/>
     </div>
   </div>;
