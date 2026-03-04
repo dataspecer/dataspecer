af6598af0f8af2e7cc7727290f08d0ea19d5017b
RadStr <radek.strycek@seznam.cz>
2026-03-04 23:17:19 +0100

    Fix build error

--- services/backend/src/routes/git/get-opened-pull-requests.ts
+++ services/backend/src/routes/git/get-opened-pull-requests.ts
@@ -12,19 +12,18 @@ import { GitProviderFactory } from "@dataspecer/git/git-providers";
  */
 export const getOpenedPullRequests = asyncHandler(async (request: express.Request, response: express.Response) => {
   const querySchema = z.object({
-    branch: z.string().min(1),
     gitUrl: z.string().min(1),
     page: z.string().min(1),
     perPage: z.string().min(1),
   });
 
   const query = querySchema.parse(request.query);
-  const { branch, gitUrl } = query;
+  const { gitUrl } = query;
   const page = Number(query.page);
   const perPage = Number(query.perPage);
 
   const gitProvider: GitProvider = GitProviderFactory.createGitProviderFromRepositoryURL(gitUrl, httpFetch, configuration);
   const { committerAccessToken } = getGitCredentialsFromSession(request, response, [ConfigType.LoginInfo, ConfigType.FullPublicRepoControl, ConfigType.DeleteRepoControl])
-  const openedPullRequests = await gitProvider.getOpenedPullRequests(gitUrl, branch, page, perPage, committerAccessToken);
+  const openedPullRequests = await gitProvider.getOpenedPullRequests(gitUrl, page, perPage, committerAccessToken);
   response.status(200).json(openedPullRequests);
 });
