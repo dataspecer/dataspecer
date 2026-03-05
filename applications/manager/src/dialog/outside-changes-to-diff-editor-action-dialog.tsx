993f6d341e2a200790933f784d74eaf9e05310c2
RadStr <radek.strycek@seznam.cz>
2026-03-05 17:05:00 +0100

    Improve the dialog that reports outside changes when in diff editor
    Not both options are red, we have moved the cause into the title of the dialog and added explanation of buttons. That being said the explanation is maybe unnecessary

--- applications/manager/src/dialog/outside-changes-to-diff-editor-action-dialog.tsx
+++ applications/manager/src/dialog/outside-changes-to-diff-editor-action-dialog.tsx
@@ -2,6 +2,7 @@ import { BetterModalProps, } from "@/lib/better-modal";
 import { Modal, ModalContent, ModalDescription, ModalFooter, ModalHeader, ModalTitle } from "@/components/modal";
 import { Button } from "@/components/ui/button";
 import { MergeState } from "@dataspecer/git";
+import { PopOverGitGeneralComponent } from "@/components/popover-git-general";
 
 export enum DiffEditorOutsideChangeChosenAction {
   Nothing,
@@ -22,33 +23,30 @@ export const ChooseActionForDiffEditorUnplannedChange = ({ oldMergeState, newMer
     resolve({ result: chosenResult });
   };
 
-
   return (
     <Modal open={isOpen} onClose={() => resolve({ result: DiffEditorOutsideChangeChosenAction.Nothing })}>
         <ModalContent>
           <ModalHeader>
-            <ModalTitle>Choose further action</ModalTitle>
-            <ModalDescription>The package was modified while you were working in the diff editor</ModalDescription>
+            <ModalTitle>Changes detected outside the editor</ModalTitle>
+            <ModalDescription>{newMergeState.isUpToDate ? <p>The modification came from another diff editor instance.</p> : <p>The modification came from another component of Dataspecer and not another instance of diff editor.</p>}</ModalDescription>
           </ModalHeader>
           <div>
-            <div className="flex flex-1 flex-row">
-              <strong>Cause:&nbsp;</strong>
-              {
-              newMergeState.isUpToDate ? <p>The merge state was modified by somebody else in different instance of diff editor.</p> : <p>The merge state was modified outside of editor.</p>
-              }
-
-            </div>
-            <br/>
-
             Working version modified at <strong>{new Date(oldMergeState.modifiedDiffTreeAt).toLocaleString()}</strong>
             <br/>
             New version modified at <strong>{new Date(newMergeState.modifiedDiffTreeAt).toLocaleString()}</strong>
+            <br/>
+            <br/>
+            <strong>Cancel</strong> closes the dialog, but keeps the diff editor unchanged.
+            <br/>
+            <strong>Discard changes</strong> discards your changes and resets the editor.
+            <br/>
+            <div className="flex flex-1 flex-row"><strong>Save changes</strong>&nbsp;saves your changes to backend.<SaveChangesTooltip/></div>
           </div>
 
           <ModalFooter>
             <Button variant="outline" onClick={() => handleReturn(DiffEditorOutsideChangeChosenAction.Nothing)}>Cancel</Button>
-            <Button variant="destructive" onClick={() => handleReturn(DiffEditorOutsideChangeChosenAction.Reload)}>Reload</Button>
-            <Button variant="default" onClick={() => handleReturn(DiffEditorOutsideChangeChosenAction.Continue)}>Save anyways</Button>
+            <Button variant="destructive" onClick={() => handleReturn(DiffEditorOutsideChangeChosenAction.Reload)}>Discard changes</Button>
+            <Button variant="destructive" onClick={() => handleReturn(DiffEditorOutsideChangeChosenAction.Continue)}>Save changes</Button>
           </ModalFooter>
         </ModalContent>
     </Modal>
@@ -56,3 +54,11 @@ export const ChooseActionForDiffEditorUnplannedChange = ({ oldMergeState, newMer
 }
 
 export const saveChangesTooltipText: string = "Only the files loaded in diff editor are saved. That is those which you touched while working in diff editor. They can be identified by the 📥 icon next to them.";
+
+function SaveChangesTooltip() {
+  return <div>
+    <PopOverGitGeneralComponent>
+      <div>{saveChangesTooltipText}</div>
+    </PopOverGitGeneralComponent>
+  </div>
+}
