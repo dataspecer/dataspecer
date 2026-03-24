172b6bf156d4be7053d561a65b120b3065e4082e
Štěpán Stenchlák <s.stenchlak@gmail.com>
2026-03-24 12:45:47 +0100

    Move pim wrapper reload to backend

--- applications/manager/src/dialog/reload-pim-wrapper.tsx
+++ applications/manager/src/dialog/reload-pim-wrapper.tsx
@@ -1,42 +1,36 @@
 import { Modal, ModalBody, ModalContent, ModalDescription, ModalFooter, ModalHeader, ModalTitle } from "@/components/modal";
 import { Button } from "@/components/ui/button";
 import { BetterModalProps } from "@/lib/better-modal";
-import { packageService } from "@/package";
+import { requestLoadPackage } from "@/package";
 import { Loader } from "lucide-react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { createRdfsModel } from "@dataspecer/core-v2/semantic-model/simplified";
 import { toast } from "sonner";
 
 export interface ReloadPimWrapperProps {
   id: string;
+  parentId: string;
 }
 
-export const ReloadPimWrapper = ({ id, isOpen, resolve }: ReloadPimWrapperProps & BetterModalProps<boolean>) => {
+export const ReloadPimWrapper = ({ id, parentId, isOpen, resolve }: ReloadPimWrapperProps & BetterModalProps<boolean>) => {
   const {t} = useTranslation();
 
   const [isLoading, setIsLoading] = useState(false);
   const doReload = async () => {
     setIsLoading(true);
 
-    try {
-      const data = await packageService.getResourceJsonData(id)! as {urls: string[]};
-      const urls = data.urls;
-      const newModel = await createRdfsModel(urls, fetch);
+    const result = await fetch(import.meta.env.VITE_BACKEND + "/resources/reload?iri=" + encodeURIComponent(id), {
+      method: "POST",
+    });
 
-      // We need to override its id
-      newModel.id = id;
-
-      await packageService.setResourceJsonData(id, newModel.serializeModel());
-    } catch (e) {
-      setIsLoading(false);
-      console.error(e);
+    if (result.ok) {
+      await requestLoadPackage(parentId, true);
+      toast.success(t("reload-pim-wrapper.success"));
+    } else {
       toast.error(t("reload-pim-wrapper.error"));
-      return;
     }
 
     resolve(true);
-    toast.success(t("reload-pim-wrapper.success"));
   }
 
   return (
