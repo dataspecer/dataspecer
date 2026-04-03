beec3e5dc10ff49ccfd055e2f0b66ed54146128c
Petr Škoda <skodapetr@gmail.com>
2026-04-03 17:30:47 +0200

    Remove unused code

--- applications/conceptual-model-editor/src/application/error.ts
+++ applications/conceptual-model-editor/src/application/error.ts
@@ -1,13 +1,3 @@
-import { Entity } from "@dataspecer/core-v2";
-
-/**
- * Base exception for runtime error.
- */
-export class RuntimeError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
 
 /**
  * @throws {RuntimeError} When assertion fail.
@@ -21,6 +11,15 @@ export function assert(
   }
   console.error("Assert failed!", { message, optionalParams });
   throw new RuntimeError(message);
+}
+
+/**
+ * Base exception for runtime error.
+ */
+class RuntimeError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
 }
 
 /**
@@ -46,16 +45,6 @@ export class UnsupportedOperationException extends RuntimeError {
 }
 
 /**
- * Used when writable model is expected but not found.
- * @deprecated Use {@link InvalidState}.
- */
-export class NoWritableModelFound extends RuntimeError {
-  constructor() {
-    super("Missing writable vocabulary.");
-  }
-}
-
-/**
  * Use when you can not find a model with a given identifier.
  * @deprecated Use {@link InvalidState}.
  */
@@ -65,47 +54,3 @@ export class MissingModel extends RuntimeError {
   }
 }
 
-/**
- * Use when there is an issue with aggregated entity, e.g.
- * the type does not match.
- * @deprecated Use {@link InvalidState}.
- */
-export class InvalidAggregation extends RuntimeError {
-
-  aggregated: Entity | null;
-
-  constructor(identifier: string, aggregated: Entity | null) {
-    super(`Invalid aggregation of entity '${identifier}'.`);
-    this.aggregated = aggregated;
-  }
-}
-
-/**
- * Use when you expect relationship to have a range but it does not have one.
- * @deprecated Use {@link InvalidState}.
- */
-export class MissingRelationshipEnds extends RuntimeError {
-  constructor(entity: Entity) {
-    super(`Missing ends for relationship '${entity.id}'.`);
-  }
-}
-
-/**
- * Use when profiled entity can not be found.
- * @deprecated Use {@link InvalidState}.
- */
-export class MissingProfile extends RuntimeError {
-  constructor(identifier: string) {
-    super(`Missing profile entity '${identifier}'.`);
-  }
-}
-
-/**
- * Use when you expect to find an entity and fail.
- * @deprecated Use {@link InvalidState}.
- */
-export class MissingEntity extends RuntimeError {
-  constructor(identifier: string) {
-    super(`Missing entity '${identifier}'.`);
-  }
-}
