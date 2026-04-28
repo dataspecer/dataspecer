c902d844c0b27290c177d601ddbbcc6294701a0a
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-04-28 16:58:11 +0200

    add ui components from manager application

--- applications/controlled-vocab-manager/src/main.tsx
+++ applications/controlled-vocab-manager/src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx'
 import './globals.css'
 import { I18nextProvider } from "react-i18next"
 import { i18nConfig } from './i18n.ts'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider attribute='class'>
       <I18nextProvider i18n={i18nConfig} defaultNS={'default'}>
-        <App />
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
       </I18nextProvider>
     </ThemeProvider>
   </React.StrictMode>,
