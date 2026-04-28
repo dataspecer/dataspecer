36a407c4c433d6fc7945c84702061ea86d84bb65
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-04-28 16:54:08 +0200

    initial setup

---
+++ applications/controlled-vocab-manager/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>Controlled Vocabulary Manager</div>
+  )
+}
+
+export default App
---
+++ applications/controlled-vocab-manager/src/main.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'next-themes'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './globals.css'
+import { I18nextProvider } from "react-i18next"
+import { i18nConfig } from './i18n.ts'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider attribute='class'>
+      <I18nextProvider i18n={i18nConfig} defaultNS={'default'}>
+        <App />
+      </I18nextProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
