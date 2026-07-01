f34e28d3e8b76d2b96fee0b2a767efded9df1ecc
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-07-01 10:54:38 +0200

    remove demo from main

--- applications/conceptual-model-editor/src/main.tsx
+++ applications/conceptual-model-editor/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Application from "./page";
-import { QualifierTestPage } from "./dev/demo-page";
 
 import "./main.css";
 
@@ -9,6 +8,6 @@ const DEV_TEST = true;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {DEV_TEST ? <QualifierTestPage /> : <Application />}
+    <Application />
   </StrictMode>,
 );
