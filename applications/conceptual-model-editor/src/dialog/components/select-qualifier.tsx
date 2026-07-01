cc29c6a53f94877fce3b6005339973b314b148a2
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-07-01 10:53:19 +0200

    fix structure  - block comment, ordering

--- applications/conceptual-model-editor/src/dialog/components/select-qualifier.tsx
+++ applications/conceptual-model-editor/src/dialog/components/select-qualifier.tsx
@@ -8,37 +8,12 @@ export enum Qualifier {
   May = "MAY",
 }
 
-const QUALIFIERS = [
-  Qualifier.Must,
-  Qualifier.AtLeastOne,
-  Qualifier.Recommended,
-  Qualifier.May,
-];
-
-const QUALIFIER_LABELS: Record<Qualifier, string> = {
-  [Qualifier.Must]: "MUST",
-  [Qualifier.AtLeastOne]: "AT LEAST 1",
-  [Qualifier.Recommended]: "RECOMMENDED",
-  [Qualifier.May]: "MAY",
-};
-
-// Stricter qualifiers come first - a qualifier is disabled if it is less strict than inherited.
-const QUALIFIER_STRICTNESS: Record<Qualifier, number> = {
-  [Qualifier.Must]: 0,
-  [Qualifier.AtLeastOne]: 1,
-  [Qualifier.Recommended]: 2,
-  [Qualifier.May]: 3,
-};
-
-function isDisabledByInherited(option: Qualifier, inherited: Qualifier): boolean {
-  return QUALIFIER_STRICTNESS[option] > QUALIFIER_STRICTNESS[inherited];
-}
-
-// Select usage qualifier for controlled vocabulary
-// If the inherited value is specified, the user can select only the options that are stricter than the inherited option 
-// - so if recommended is selected, the may option is disabled, the other 3 are enabled. 
-// If the value is different from inherited, the inherited option is highlighted visually.
-
+/**
+ * Select usage qualifier for controlled vocabulary
+ * If the inherited value is specified, the user can select only the options that are stricter than the inherited option 
+ * - so if recommended is selected, the may option is disabled, the other 3 are enabled. 
+ * If the value is different from inherited, the inherited option is highlighted visually.
+ */
 export function SelectQualifier(props: {
   value: Qualifier | null,
   inherited?: Qualifier | null,
@@ -86,3 +61,31 @@ export function SelectQualifier(props: {
     </fieldset>
   );
 }
+
+
+const QUALIFIERS = [
+  Qualifier.Must,
+  Qualifier.AtLeastOne,
+  Qualifier.Recommended,
+  Qualifier.May,
+];
+
+const QUALIFIER_LABELS: Record<Qualifier, string> = {
+  [Qualifier.Must]: "MUST",
+  [Qualifier.AtLeastOne]: "AT LEAST 1",
+  [Qualifier.Recommended]: "RECOMMENDED",
+  [Qualifier.May]: "MAY",
+};
+
+// Stricter qualifiers come first - a qualifier is disabled if it is less strict than inherited.
+const QUALIFIER_STRICTNESS: Record<Qualifier, number> = {
+  [Qualifier.Must]: 0,
+  [Qualifier.AtLeastOne]: 1,
+  [Qualifier.Recommended]: 2,
+  [Qualifier.May]: 3,
+};
+
+function isDisabledByInherited(option: Qualifier, inherited: Qualifier): boolean {
+  return QUALIFIER_STRICTNESS[option] > QUALIFIER_STRICTNESS[inherited];
+}
+
