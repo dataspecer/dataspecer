12a94db8a52551f3b9ca1b573a93e29c6cb08e46
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-07-01 13:13:08 +0200

    add doc comment

--- packages/controlled-vocabulary-model/src/concepts/controlled-vocabulary.ts
+++ packages/controlled-vocabulary-model/src/concepts/controlled-vocabulary.ts
@@ -2,11 +2,14 @@ import type { Entity } from "@dataspecer/core/entity-model";
 
 export const CONTROLLED_VOCABULARY_TYPE = "controlled-vocabulary" as const;
 
-export interface ControlledVocabularyDistribution {
-  downloadUrl: string;
-  accessUrl: string;
-}
-
+/**
+ * Interface representing metadata identified about a controlled vocabulary
+ * - title = name of the controlled vocabulary (CV)
+ * - pattern = regex pattern of the IRIs of CV values
+ * - references = IRI of the CV that the metadata references (IRI of skos:ConceptScheme)
+ * - documentation = documentation URL
+ * - distribution = reference to the CV distribution
+ */
 export interface ControlledVocabulary extends Entity {
   type: [typeof CONTROLLED_VOCABULARY_TYPE];
   title: string;
@@ -14,6 +17,17 @@ export interface ControlledVocabulary extends Entity {
   references: string;
   documentation: string;
   distribution: ControlledVocabularyDistribution;
+}
+
+/**
+ * Represents the CV distribution - point of access to the CV raw data
+ * Based on DCAT specification - accessUrl is a required parameter
+ * (distribution can be not downloadable - like endpoint)
+ * for most CVs downloadableUrl will match accessUrl
+ */
+export interface ControlledVocabularyDistribution {
+  downloadUrl: string;
+  accessUrl: string;
 }
 
 export function isControlledVocabulary(entity: Entity): entity is ControlledVocabulary {
