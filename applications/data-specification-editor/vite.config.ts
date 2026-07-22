63a456c63082b8d52a59663b55fe8de7d362b583
Štěpán Stenchlák <s.stenchlak@gmail.com>
2026-07-22 11:52:07 +0200

    Fix history app base url

--- applications/data-specification-editor/vite.config.ts
+++ applications/data-specification-editor/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 import i18nextLoader from 'vite-plugin-i18next-loader'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
--- applications/history/vite.config.ts
+++ applications/history/vite.config.ts
@@ -1,32 +1,36 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import i18nextLoader from 'vite-plugin-i18next-loader'
 
-export default defineConfig({
-  server: {
-    port: 5177,
-  },
-  base: "",
-  plugins: [
-    react(),
-    tailwindcss(),
-    i18nextLoader({
-      paths: ['./locales'],
-      namespaceResolution: 'basename'
-    })
-  ],
-  build: {
-    commonjsOptions: {
-      include: [/packages\//, /node_modules/],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: 5177,
     },
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "fs": "path", // Hack to make polyfill to null
+    base: env.VITE_BASE_PATH ?? "/",
+    plugins: [
+      react(),
+      tailwindcss(),
+      i18nextLoader({
+        paths: ['./locales'],
+        namespaceResolution: 'basename'
+      })
+    ],
+    build: {
+      commonjsOptions: {
+        include: [/packages\//, /node_modules/],
+      },
+      sourcemap: true,
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "fs": "path", // Hack to make polyfill to null
+      },
+    },
+  };
+});
