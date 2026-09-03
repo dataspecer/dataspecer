74c51ea5f950d44ea40fb6db6fa074604c77bee7
Štěpán Stenchlák <s.stenchlak@gmail.com>
2026-09-03 14:29:22 +0200

    npx knip

--- packages/core/src/pim/adapter/rdf/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./pim-association-adapter.ts";
-export * from "./pim-association-end-adapter.ts";
-export * from "./pim-attribute-adapter.ts";
-export * from "./pim-class-adapter.ts";
-export * from "./pim-schema-adapter.ts";
