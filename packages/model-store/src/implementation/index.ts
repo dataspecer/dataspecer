52c13813e7c4052ff8da114eb9ba55928597a9dd
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-09-13 16:46:25 +0200

    create a model store for CVs

--- packages/model-store/src/implementation/index.ts
+++ packages/model-store/src/implementation/index.ts
@@ -6,7 +6,7 @@ export {
   resolveAsyncQueryableModelEntities,
   serializationToAsyncQueryableModelEntities,
 } from "./async-queryable-model.ts";
-export { createCMEModelStore, createDSEModelStore, createManagerModelStore } from "./factories.ts";
+export { createCMEModelStore, createControlledVocabularyManagerModelStore, createDSEModelStore, createManagerModelStore } from "./factories.ts";
 export { DefaultFrontendModelStore, type ModelInModelStoreBuilder } from "./implementation.ts";
 export { getModelMetadata } from "./metadata.ts";
 export { type MainEntity, ReloadModelOperationType, SetModelUrlsOperationType, type SetModelUrl } from "./pim-model.ts";
